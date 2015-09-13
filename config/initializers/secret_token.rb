5c22b20dfa1d5448ee3d3bc4e0d3e317b43df85e
Hannah Stucken Douglas <h.s.douglas@gmail.com>
2015-09-13 12:15:19 -0400

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShiftExchange::Application.config.secret_key_base = 'a3ef641be11430dcab0d317ddfc0535c8d9411eb64c53daa56c1264c17dabd0c647cf822e966942de4683cad5706e808af35693b4ce3ed18020889d2fb709c7a'
