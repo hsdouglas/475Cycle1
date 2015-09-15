263ec698a6ad6cd3c82472dbb6323b84087c3b81
dtsong <dtsong@andrew.cmu.edu>
2015-09-15 14:00:16 -0400

    swap and substitute functions now create closed_shifts to keep shift history

--- app/models/closed_shift.rb
+++ app/models/closed_shift.rb
@@ -1,9 +1,4 @@
 class ClosedShift < ActiveRecord::Base
 	belongs_to :open_shift
 	has_many :notifications
-
-	def create_swapped_closed_shifts(shiftA, shiftB)
-		CloseShift.new(open_shift_id: shiftA.id, new_assignee_id: shiftB.assignee_id, claimed: true)
-		CloseShift.new(open_shift_id: shiftB.id, new_assignee_id: shiftA.assignee_id, claimed: true)
-	end
 end
